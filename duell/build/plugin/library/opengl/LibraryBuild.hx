1c0e3268d712d847d12aaf103da2e93ec43568bd
kgar <khaled.garbaya@gamduell.de>
2014-09-24 17:22:03 +0200

    code clean up

--- duell/build/plugin/library/opengl/LibraryBuild.hx
+++ duell/build/plugin/library/opengl/LibraryBuild.hx
@@ -6,23 +6,6 @@
 
 package duell.build.plugin.library.opengl;
 
-/*
-import duell.build.objects.DuellProjectXML;
-import duell.build.objects.Configuration;
-import duell.build.helpers.TemplateHelper;
-import duell.build.helpers.XCodeHelper;
-
-import duell.helpers.PathHelper;
-import duell.helpers.LogHelper;
-import duell.helpers.FileHelper;
-import duell.helpers.ProcessHelper;
-
-import duell.objects.DuellLib;
-import duell.objects.Haxelib;
-
-import sys.FileSystem;
-import haxe.io.Path;
-*/
 import duell.build.objects.Configuration;
 
  import duell.objects.DuellLib;
